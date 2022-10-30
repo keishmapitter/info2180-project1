077346193a27384056fdfc59dd90139a2e23ab7c
keishma pitter <pitterkeishma@gmail.com>
2022-10-30 16:04:33 -0500

    Edited webpage files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 /* Add your JavaScript to this file */
+windows.onload = funtion() 
+{
+  let button = document.getElementsByClassName("button")[0];
+  button.addEventListener("click", function(e){
+     e.preventDefault();
+  var enter_email = document.getElementById("email");
+  var enter_message = document.getElementsByClassName("message")[0];
+
+  if (enter_email.value != ""){
+      enter_message.textContent = "Your email address has been added, Thank you for your time"
+  }    
+  else
+  {
+      enter_message.textContent = "Your email is invalid, Please enter a valid email address."
+  }
+
+     } 
+ )}
